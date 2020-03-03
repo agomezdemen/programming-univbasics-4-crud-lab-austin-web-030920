7274248bdc0c37f70a1e108e19d02734c46193bc
munificent-cast-9719 <agomezdemen15@gmail.com>
2020-03-03 17:48:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,9 +31,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  p array[array.length - 1]
+  p array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  
 end
